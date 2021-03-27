77f76b12967eafbf344d57171b747f2416110b06
suphasonnuk <suphasonnuk@gmail.com>
2021-03-28 00:17:09 +0700

    Fix token balanace

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -60,7 +60,7 @@ const Home: React.FC = () => {
         <Heading as="h1" size="xl" mb="24px" color="secondary">
           {TranslateString(576, 'Estate-OnBlock')}
         </Heading>
-        <Text>{TranslateString(578, 'Top 3 best DEFI app on Binance Smart Chain.')}</Text>
+        <Text>{TranslateString(578, '')}</Text>
       </Hero>
       <div>
         <Cards>
