3734a8f6d92f8ccf954ccba76aa89d9c0a37e7a2
suphasonnuk <suphasonnuk@gmai.com>
2021-05-26 16:09:23 +0700

    ngen-frontend

--- src/views/Home/components/TwitterCard.tsx
+++ src/views/Home/components/TwitterCard.tsx
@@ -35,7 +35,7 @@ const TwitterCard = () => {
         <Timeline
           dataSource={{
             sourceType: 'profile',
-            screenName: 'bts_bighit'
+            screenName: 'FinanceNaga'
           }}
           options={{
             height: '300',
