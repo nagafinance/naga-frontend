39054a9f815b6be4776857e9e721a49f78742d80
suphasonnuk <suphasonnuk@gmai.com>
2021-07-04 00:59:11 +0700

    img edited

--- src/naga-uikit/widgets/Menu/icons/Logo.tsx
+++ src/naga-uikit/widgets/Menu/icons/Logo.tsx
@@ -9,8 +9,8 @@ interface LogoProps extends SvgProps {
 const Logo: React.FC<LogoProps> = ({ isDark, ...props }) => {
   const textColor = isDark ? "#FFFFFF" : "#000000";
   return (
-    <Svg viewBox="0 0 205 26" {...props}>
-      <image width="205" height="26" href={isDark ? '/images/egg/LogoTextNewDark.png' : '/images/egg/LogoTextNewWhite.png'}/>
+    <Svg viewBox="0 0 300 55" {...props}>
+      <image width="300" height="55" href={isDark ? '/images/egg/LogoTextNewDark.png' : '/images/egg/LogoTextNewWhite.png'} />
     </Svg>
   )
 };
